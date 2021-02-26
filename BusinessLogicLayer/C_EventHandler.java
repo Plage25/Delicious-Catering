a6831e30248c537fe33ecc9abef3babbcba0dabf
Name & Surname <b.plage@hotmail.co.za>
2021-02-26 13:40:59 +0200

    Fixed multi layered architecture

---
+++ BusinessLogicLayer/C_EventHandler.java
@@ -0,0 +1,9 @@
+package BusinessLogicLayer;
+
+import DataAccessLayer.C_Database;
+
+public class C_EventHandler extends C_Database {
+
+   
+    
+}
---
+++ PresentationLayer/UserInput.java
@@ -0,0 +1,16 @@
+package PresentationLayer;
+
+
+import BusinessLogicLayer.C_EventHandler;
+
+public class UserInput extends C_EventHandler {
+    
+    public static void main(String[] args) {
+        
+     
+    }
+
+   
+
+    
+}
