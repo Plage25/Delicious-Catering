44c25face4f53b80ad8584792586b32289ac8f93
Stef98 <stefferreira14@gmail.com>
2021-03-04 15:40:38 +0200

    Database update

--- BusinessLogicLayer/C_EventHandler.java
+++ BusinessLogicLayer/C_EventHandler.java
@@ -1,6 +1,7 @@
 package BusinessLogicLayer;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import DataAccessLayer.C_Database;
 
@@ -20,20 +21,134 @@ public class C_EventHandler extends C_Database {
     }
 
     public static boolean Register() {
-        System.out.println("Enter Name");
-        String userName = input.nextLine(); // Read user input
-        System.out.println("Enter Surname");
-        String userSurname = input.nextLine(); // Read user input
-        System.out.println("Enter Email");
-        String userEmail = input.nextLine(); // Read user input
-        System.out.println("Enter Phone Number");
-        String userPhoneNo = input.nextLine(); // Read user input
-        System.out.println("Enter Password");
-        String userPassword = input.nextLine(); // Read user input
-        System.out.println("Enter Your Password again");
-        String userPasswordMatch = input.nextLine(); // Read user input
+        int IncorrectDetails = 1;
 
+        while (IncorrectDetails == 1) {
+
+            System.out.println("======Register Menu======");
+            System.out.println("Enter Name");
+            String userName = input.nextLine(); // Read user input
+            System.out.println("Enter Surname");
+            String userSurname = input.nextLine(); // Read user input
+
+            if (!(userName.isEmpty()) && !(userSurname.isEmpty())) {// Upcoming are use of nested if's
+                System.out.println("Enter Email");
+                String userEmail = input.nextLine(); // Read user input
+
+                if (isValidEmail(userEmail)) {
+                    System.out.println("Enter Phone Number");
+                    String userPhoneNo = input.nextLine(); // Read user input
+                    if ((userPhoneNo.length() == 10) && (userPhoneNo.startsWith("0"))
+                            && (onlyDigits(userPhoneNo) == true)) {
+                        System.out.println("Enter Password");
+                        String userPassword = input.nextLine(); // Read user input
+                        System.out.println("Enter Your Password again");
+                        String userPasswordMatch = input.nextLine(); // Read user input
+                        if ((userPassword.equals(userPasswordMatch)) && !(userPassword.isEmpty())
+                                && !(userPasswordMatch.isEmpty())) {
+                            IncorrectDetails = 0;
+                            C_Database C_DBD = new C_Database();
+                            C_DBD.Register(userName, userSurname, userEmail, userPassword, userPhoneNo);
+                        } else {
+
+                            System.out.print("\033[H\033[2J");
+                            System.out.flush();
+                            System.out.println("Passwords do not match\n");
+                        }
+                    } else {
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                        System.out.println("Number format not correct!Try again\n");
+                    }
+
+                } else {
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println("Email address format not correct!Try again\n");
+                    ;
+                }
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                System.out.println("Username or surname format not correct!Try again");
+            }
+
+        }
         return true;
+    }
+
+    public static boolean isValidEmail(String email) { // Check if email is correct format
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z"
+                + "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
+    public static boolean onlyDigits(String str) // Check if number format input correct
+    {
+        for (int i = 0; i < 10; i++) { // Ensure number must be 10 digits long containing only digits
+
+            if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static boolean PlaceOrder() {
+
+        int j = 1;
+        while (j == 1) { // While loop Welcome page
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println("------------------------------");
+            System.out.println("Welcome to Delicious Catering!");
+            System.out.println("-----Choose option below------");
+            System.out.println("1. Event Details");
+            System.out.println("2. Food Catering");
+            System.out.println("3. Drinks Catering");
+            System.out.println("4. Decore");
+            System.out.println("5. Return");
+
+            int Choice = input.nextInt();
+            switch (Choice) {
+                case 1:
+                    System.out.println("PLease Enter the events infomation below");
+                    System.out.println("Type of Party: ");
+                    String EventType = input.next(); // Read user input
+                    System.out.println("Enter the date of the event: ");
+                    String EventDate = input.next(); // Read user input
+                    System.out.println("Please enter the events Address: ");
+                    String EventAdress = input.next(); // Read user input
+                    System.out.println("Please enter a contact number for the event: ");
+                    String ContactNumber = input.next(); // Read user input
+                    break;
+                case 2:
+                    System.out.println("Please enter the amount of adults that will be at the event: ");
+                    String AmountOfAdults = input.next(); // Read user input
+                    System.out.println("Please enter the amount of Kids that will be at the event: ");
+                    String AmountOfKids = input.next(); // Read user input
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    j = 0;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        return false;
     }
 
     public static void ForgotPassword() {
