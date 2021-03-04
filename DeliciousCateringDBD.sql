3e4421b7e00c2c6ce21b4bea98e5d19afb80294d
Stef98 <stefferreira14@gmail.com>
2021-03-04 15:42:23 +0200

    Database Edit

--- DeliciousCateringDBD.sql
+++ DeliciousCateringDBD.sql
@@ -12,7 +12,7 @@ GO
 
 CREATE TABLE Client
 (
-[ClientID] int PRIMARY KEY,
+[ClientID] int PRIMARY KEY IDENTITY(1,1),
 [FirstName] VARCHAR(50) NOT NULL, --Name of customer
 [LastName] VARCHAR(50) NOT NULL,  --Surname of customer
 [Email] VARCHAR(100) NOT NULL,  --Email of customer
@@ -23,7 +23,7 @@ GO
 
 CREATE TABLE Menu
 (
-[FoodID] int PRIMARY KEY, --FoodID
+[FoodID] int PRIMARY KEY IDENTITY(1,1), --FoodID
 [Type] VARCHAR(20) NOT NULL, --Adult,Kids,Drinks,Dessert
 [Price] MONEY NOT NULL, --Price per food type
 )
@@ -31,7 +31,7 @@ GO
 
 CREATE TABLE EventDetails
 (
-EventID int PRIMARY KEY,
+EventID int PRIMARY KEY IDENTITY(1,1),
 [ClientID] int FOREIGN KEY REFERENCES Client(ClientID),
 [Type] VARCHAR(20),  --Type of event Example; Wedding, Barbeque
 [Date] DATE,  --Date of event
@@ -53,23 +53,23 @@ GO
 
 INSERT INTO Client 
 VALUES
-(1, 'Brent', 'Plage', 'Plage@gmail.com', 'plagemaster', '0866598745'),
-(2, 'Claude', 'Pretorius', 'ClaudeP@gmail.com', 'pretoriusmaster', '0866598745')
+('Brent', 'Plage', 'Plage@gmail.com', 'plagemaster', '0866598745'),
+('Claude', 'Pretorius', 'ClaudeP@gmail.com', 'pretoriusmaster', '0866598745')
 GO
 
 
 INSERT INTO Menu
 VALUES
-(1,'Adult-Meal', 20),
-(2,'Kid-Meal', 15),
-(3,'Drink', 11),
-(4,'Dessert', 15)
+('Adult-Meal', 20),
+('Kid-Meal', 15),
+('Drink', 11),
+('Dessert', 15)
 GO
 
 INSERT INTO EventDetails
 values
-(1,2,'Wedding','2021/12/17', '13:30', '56 Platberg Avenue, Kempton Park, Gauteng', 10, 5, 8, 4, 15, 15,'Deposit', 253,506, 'N/a', 0),
-(2,1,'Barbeque','2021/12/18', '11:25', '84 Johnson Avenue, Boksburg, Gauteng', 11, 9, 11, 9, 20, 20 ,'Yes', 0, 600, 'Yes- 50 blue balloons on the front gate and 10 surprise wrappers per meal with a toy in', 50)
+(2,'Wedding','2021/12/17', '13:30', '56 Platberg Avenue, Kempton Park, Gauteng', 10, 5, 8, 4, 15, 15,'Deposit', 253,506, 'N/a', 0),
+(1,'Barbeque','2021/12/18', '11:25', '84 Johnson Avenue, Boksburg, Gauteng', 11, 9, 11, 9, 20, 20 ,'Yes', 0, 600, 'Yes- 50 blue balloons on the front gate and 10 surprise wrappers per meal with a toy in', 50)
 GO
 
 SELECT * FROM Client
